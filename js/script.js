4b13b0c7b5473ddc141969ee1efa1795f8217840
bealabea <beatricemarin89@yahoo.it>
2021-12-06 12:58:53 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,194 @@
+const day = new Date();
+const date = day.getDate();
+
+let day1 = document.getElementById('text1');
+let num1 = day1 = 1;
+
+if (num1 === date) {
+    text1.style.display= "block";
+    img1.style.display= "none";
+}
+
+let day2 = document.getElementById('text2');
+let num2 = day2 = 2;
+
+if (num2 === date) {
+    text2.style.display= "block";
+    img2.style.display= "none";
+}
+
+let day3 = document.getElementById('text3');
+let num3 = day3 = 3;
+
+if (num3 === date) {
+    text3.style.display= "block";
+    img3.style.display= "none";
+}
+
+let day4 = document.getElementById('text4');
+let num4 = day4 = 4;
+
+if (num4 === date) {
+    text4.style.display= "block";
+    img4.style.display= "none";
+}
+
+let day5 = document.getElementById('text5');
+let num5 = day5 = 5;
+
+if (num5 === date) {
+    text5.style.display= "block";
+    img5.style.display= "none";
+}
+
+let day6 = document.getElementById('text6');
+let num6 = day6 = 6;
+
+if (num6 === date) {
+    text6.style.display= "block";
+    img6.style.display= "none";
+}
+
+let day7 = document.getElementById('text7');
+let num7 = day7 = 7;
+
+if (num7 === date) {
+    text7.style.display= "block";
+    img7.style.display= "none";
+}
+
+let day8 = document.getElementById('text8');
+let num8 = day8 = 8;
+
+if (num8 === date) {
+    text8.style.display= "block";
+    img8.style.display= "none";
+}
+
+let day9 = document.getElementById('text9');
+let num9 = day9 = 9;
+
+if (num9 === date) {
+    text9.style.display= "block";
+    img9.style.display= "none";
+}
+
+let day10 = document.getElementById('text10');
+let num10 = day10 = 10;
+
+if (num10 === date) {
+    text10.style.display= "block";
+    img10.style.display= "none";
+}
+
+let day11 = document.getElementById('text11');
+let num11 = day11 = 11;
+
+if (num11 === date) {
+    text11.style.display= "block";
+    img11.style.display= "none";
+}
+
+let day12 = document.getElementById('text12');
+let num12 = day12 = 12;
+
+if (num12 === date) {
+    text12.style.display= "block";
+    img12.style.display= "none";
+}
+
+let day13 = document.getElementById('text13');
+let num13 = day13 = 13;
+
+if (num13 === date) {
+    text13.style.display= "block";
+    img13.style.display= "none";
+}
+
+let day14 = document.getElementById('text14');
+let num14 = day14 = 14;
+
+if (num14 === date) {
+    text14.style.display= "block";
+    img14.style.display= "none";
+}
+
+let day15 = document.getElementById('text15');
+let num15 = day15 = 15;
+
+if (num15 === date) {
+    text15.style.display= "block";
+    img15.style.display= "none";
+}
+
+let day16 = document.getElementById('text16');
+let num16 = day16 = 16;
+
+if (num16 === date) {
+    text16.style.display= "block";
+    img16.style.display= "none";
+}
+
+let day17 = document.getElementById('text17');
+let num17 = day17 = 17;
+
+if (num17 === date) {
+    text17.style.display= "block";
+    img17.style.display= "none";
+}
+
+let day18 = document.getElementById('text18');
+let num18 = day18 = 18;
+
+if (num18 === date) {
+    text18.style.display= "block";
+    img18.style.display= "none";
+}
+
+let day19 = document.getElementById('text19');
+let num19 = day19 = 19;
+
+if (num19 === date) {
+    text19.style.display= "block";
+    img19.style.display= "none";
+}
+
+let day20 = document.getElementById('text20');
+let num20 = day20 = 20;
+
+if (num20 === date) {
+    text20.style.display= "block";
+    img20.style.display= "none";
+}
+
+let day21 = document.getElementById('text21');
+let num21 = day21 = 21;
+
+if (num21 === date) {
+    text21.style.display= "block";
+    img21.style.display= "none";
+}
+
+let day22 = document.getElementById('text22');
+let num22 = day22 = 22;
+
+if (num22 === date) {
+    text22.style.display= "block";
+    img22.style.display= "none";
+}
+
+let day23 = document.getElementById('text23');
+let num23 = day23 = 23;
+
+if (num23 === date) {
+    text23.style.display= "block";
+    img23.style.display= "none";
+}
+
+let day24 = document.getElementById('text24');
+let num24 = day24 = 24;
+
+if (num24 === date) {
+    text24.style.display= "block";
+    img24.style.display= "none";
+}
